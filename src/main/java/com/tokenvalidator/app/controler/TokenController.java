c9a513c841f3059c74e829201b4d7f5c2175bc81
lucascoutoandrade <40430653+lucascoutoandrade@users.noreply.github.com>
2024-07-14 10:55:19 -0300

    corrigido onde localizar a classe AppApplication

--- src/main/java/com/tokenvalidator/app/controler/TokenController.java
+++ src/main/java/com/tokenvalidator/app/controler/TokenController.java
@@ -3,13 +3,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody ;
 import org.springframework.web.bind.annotation.RestController;
 import com.tokenvalidator.app.model.Token;
+import com.tokenvalidator.app.service.ValidarJWT;
 
 @RestController
 public class TokenController {
 	
 //	private static final Pattern NOME_PATTERN = Pattern.compile("\\D+");
 //	private static final Pattern REGRA_PATTERN = Pattern.compile("^(Admin|Member|External)$");
-
+	ValidarJWT validarJWT = new ValidarJWT();
 	@PostMapping(value="/validate")
 	public String validate( @RequestBody Token token) {
 
@@ -21,8 +22,9 @@ public class TokenController {
 		//System.out.println(token.getValue());
 		
 		
+		
 
-		return "false";
+		return "teste";
 	}
 	
 	
